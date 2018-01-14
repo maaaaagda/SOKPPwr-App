3ae4f95fb79dd1dca0399529358b1790813f19de
maaaaagda <magdapolak96@gmail.com>
2018-01-14 19:16:53 +0100

    slowly finishing second_add

--- routes/second_add.js
+++ routes/second_add.js
@@ -2,11 +2,9 @@ var express = require('express');
 var router = express.Router();
 var sess;
 var Promise = require('bluebird');
-
+var userID = 1; //when no errors change it to sess.userID
 function renderFormData(req, res, renderData) {
   sess = req.session;
-  var userID = 1; //when no errors change it to sess.userID
-  console.log("idi "+sess.userID);
   var getConn = Promise.promisify(req.getConnection, {context: req});
   getConn().then(function(connection) {
             var query = Promise.promisify(connection.query, {context: connection});
@@ -15,7 +13,6 @@ function renderFormData(req, res, renderData) {
             console.log("Error in performing mysql query : %s " + err);
             res.statusCode = 404; // upsssssss sth went wrong
           }).then(function(rows) {
-            console.log(rows);
             renderData["inquiries"] = JSON.stringify(rows);
             renderData["inq_len"] = rows.length;
             res.render('second_add', renderData);
@@ -30,10 +27,27 @@ router.post('/', function(req, res, next) {
   var input = JSON.parse(JSON.stringify(req.body));
   sess = req.session;
   var data = sess.data;
+  data.teacherName = input.teacherName;
+  data.teacherSurname = input.teacherSurname;
+  sess.data = data;
+  var getConn = Promise.promisify(req.getConnection, {context: req});
+  getConn().then(function(connection) {
+            var query = Promise.promisify(connection.query, {context: connection});
+            return query("SELECT n.ID as teacherID FROM wynikzapytania wz JOIN nauczycielakademicki n ON wz.ProwadzacyID = n.ID WHERE wz.StudentID='"+userID+"' AND n.Imie='"+data.teacherName+"' AND n.Nazwisko='"+userID+"' LIMIT 1");
+            },function(err) {
+              console.log("Error in performing mysql query : %s " + err);
+              res.statusCode = 404; // upsssssss sth went wrong
+            }).then(function(rows) {
+              if(rows[0]) {
+                data.teacherID = rows[0].teacherID;
+              }
+              else {
+                data.teacherID = "";
+              }
+              sess.data = data;
+              res.redirect('/second_add');
+              })
 
-  console.log(sess.data);
-  console.log("id "+sess.userID);
-  res.redirect('/second_add');
 });
 
 module.exports = router;
